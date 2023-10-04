33fa57fafa35e9f9a352799e4e6671c4c703d80b
BishalDey908 <ronydey34567@gmail.com>
2023-10-04 21:19:09 +0530

    2nd commit

--- toDoList/src/components/tasks.jsx
+++ toDoList/src/components/tasks.jsx
@@ -5,11 +5,19 @@ export default function Tasks(props){
     return(
         <>
         <div className="flex flex-row xl:my-8 xl:w-[456px] md:my-8 my-4 bg-slate-400 w-80 place-content-between py-2 px-2 rounded">
+
         <li className="text-2xl md:text-3xl ">{props.text} </li>   {/*props use */}
         <div className=" md:mt-2 mt-1 flex-2">
+
+
         <i className="fa-solid fa-trash fa-xl mr-2 " onClick={()=>{
             props.onSelect(props.id)  //deleteItems(id)
         }}></i>
+
+        <i class="fa-solid fa-pen-to-square" onClick={()=>{
+            props.onSelect1(props.id) 
+        }}></i>
+
         </div >
         </div>
         </>
--- toDoList/src/components/todo.jsx
+++ toDoList/src/components/todo.jsx
@@ -5,6 +5,7 @@ export default function Todo() {
 
     const [input, setInput] = useState("")
     const [tasklist, settaskList] = useState([])
+    const [index2, setIndex2] = useState("")
 
 
     // -------------------------InsertOpearation start--------------------------------------
@@ -26,42 +27,46 @@ export default function Todo() {
     // -------------------------deleteOpearation start--------------------------------------
 
 
-    function deleteItems(id) { //deleteItems(id)
-        console.log("del")
-
-        settaskList((oldItems) => {                       //access old elements in the array
-            return oldItems.filter((arrElem, index) => {  //do filter and filter takes 2 option (element and indexnumber)
-                return index !== id                     //if (index === id ? not delete) else delete  
-            })
-        })
-    }
+    
 
     // -------------------------deleteOpearation end--------------------------------------
 
+    function edit1(id) {
+        setIndex2(id)
+        tasklist.forEach((p, index) => {
+            if (index === index2) {
+                setInput(p)
+                console.log(tasklist[index] = input)
+            }
+        })
+    }
+
+
     return (
         <>
-        <div className=" w-auto">
-            <div className="mt-6 flex place-content-center ml-2">
-                <input className="h-10 w-96 text-center rounded-full" type="text" placeholder="Input text here" value={input} onChange={showInput} />
+            <div className=" w-auto">
+                <div className="mt-6 flex place-content-center ml-2">
+                    <input className="h-10 w-96 text-center rounded-full" type="text" placeholder="Input text here" value={input} onChange={showInput} />
+                    
+                    <button className="mx-2 bg-orange-500 w-20 rounded-full" onClick={listItems}>Add</button>
+                    <hr />
+                </div>
 
-                <button className="mx-2 bg-orange-500 w-20 rounded-full" onClick={listItems}>Add</button>
-                <hr />
-            </div>
-            
-            <div className="flex place-content-center">
-            <ul>
-                {
-                    tasklist.map((items, index) => {          //set the items one by one to the list by "props"
-                        return <Tasks
-                            id={index}
-                            key={index}
-                            text={items}
-                            onSelect={deleteItems}
-                        />   // "props => text"
-                    })
-                }
-            </ul>
-            </div>
+                <div className="flex place-content-center">
+                    <ul>
+                        {
+                            tasklist.map((items, index) => {          //set the items one by one to the list by "props"
+                                return <Tasks
+                                    id={index}
+                                    key={index}
+                                    text={items}
+                                    onSelect={deleteItems}
+                                    onSelect1={edit1}
+                                />   // "props => text"
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         </>
     )
